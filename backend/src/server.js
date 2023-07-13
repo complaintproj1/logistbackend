0a3bdc3529074f78e86e8f89acbdcd5e82d552c2
complaintwork2 <137710711+complaintwork2@users.noreply.github.com>
2023-07-13 11:41:26 +0530

    nice

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const PORT = process.env.PORT || 8086
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+var routes = require('../routes/routes');
+const cors = require('cors');
+
+dotenv.config()
+
+app.use(cors(
+    {
+      origin: "http://localhost:4200"
+    }
+   
+  ));
+ 
+  mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connection Successful")
+}).catch((err)=>console.log(err));
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`)
+})
+
+app.use(express.json());
+
+app.use(routes);
