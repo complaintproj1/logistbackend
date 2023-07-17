72f798bd7d2a633e52e6326086ca5b329695f130
complaintwork2 <137710711+complaintwork2@users.noreply.github.com>
2023-07-17 21:50:52 +0530

    commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,98 @@
-const express = require('express');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const router = express.Router();
-var employeeController = require('../src/employee/employeeController');
+router.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const record = await User.findOne({ email: req.body.email });
 
-// router.post('/user/create', function(req, res) { employeeController.createUserControllerFn });
-router.post('/user/create', function(req, res) {
-    employeeController.createUserControllerFn(req, res);
+  if (record) {
+    return res.status(400).send({
+      message: "Email is already registered",
+    });
+  } else {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const result = await user.save();
+
+    const { _id } = await result.toJSON();
+
+    const token = jwt.sign({ _id: _id }, "secret");
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    res.send({
+      message: "success",
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(404).send({
+      message: "User not Found",
+    });
+  }
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(400).send({
+      message: "Password is Incorrect",
+    });
+  }
+
+  const token = jwt.sign({ _id: user._id }, "secret");
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
-  router.get('/user/findOne', function(req, res) {
-    employeeController.findOneUserController(req, res);
-  });
 
- 
+  res.send({
+    message: "success",
+  });
+});
+
+router.get("/user", async (req, res) => {
+  try {
+    const cookie = req.cookies["jwt"];
+
+    const claims = jwt.verify(cookie, "secret");
+
+    if (!claims) {
+      return res.status(401).send({
+        message: "unauthenticated",
+      });
+    }
+
+    const user = await User.findOne({ _id: claims._id });
+
+    const { password, ...data } = await user.toJSON();
+
+    res.send(data);
+  } catch (e) {
+    return res.status(401).send({
+      message: "unauthenticated",
+    });
+  }
+});
+
+router.post("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+
+  res.send({
+    message: "success",
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+
 const dotenv = require('dotenv')
-const PORT = process.env.PORT || 8086
+const PORT = process.env.PORT || 8000
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-var routes = require('./routes/routes');
-const cors = require('cors');
+
+const routes = require('./routes/routes')
+
+const cors = require('cors')
+
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
-const allowedOrigins = ['https://daakgadi.web.app']; // Add your Firebase Hosting URL here
 
-// CORS configuration
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  })
-);  mongoose.connect(process.env.MONGO_URL).then(()=>{
+const app = express()
+
+
+
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  }));
+app.use(cookieParser())
+
+app.use(express.json())
+app.use('/api', routes)
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connection Successful")
 }).catch((err)=>console.log(err));
 
 
-
 app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`)
-})
+    console.log(`Server Running on Port ${PORT}`)
+  })
 
+  
 app.use(express.json());
 
-app.use(routes);
